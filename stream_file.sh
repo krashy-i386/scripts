15849aed5183e6cadeb01074378e0e5c7fa72e25
krashy-i386 <riccardoromagnoli06@gmail.com>
2022-02-07 17:15:59 +0100

    simple script to stream a file with ffmpeg to rtmp

--- stream_file.sh
+++ stream_file.sh
@@ -25,6 +25,6 @@ ffmpeg \
 	-re -vsync -1 -i "$IN" $OPTS \
 	-vcodec $VC -preset $QUAL -pix_fmt yuv420p -vf scale=$RES -r $FPS -g $(($FPS * 2)) -b:v $VBR \
 	-acodec $AC -ar 44100	-b:a $ABR -bufsize 512K \
-	-f flv "$URL/$KEY"
+	-preset ultrafast -f flv "$URL/$KEY"
 
 notify-send "🎥 Live Stream Ended 🔴"
